760817541e36bb3b58cd44bdb7a5e2a41cc73fbe
Dami Kim <dami.k.dev@gmail.com>
2021-10-07 01:33:20 +0900

    style: App arrow function으로 코드 수정

--- mobx-todo/src/App.tsx
+++ mobx-todo/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Main from "./pages/Main";
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <Main />
     </div>
   );
-}
+};
 
 export default App;
