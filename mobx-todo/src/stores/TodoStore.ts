b16bc79ad2f35ee7d3706977e50edfa27f5a9ba7
Dami Kim <dami.k.dev@gmail.com>
2021-10-07 00:18:10 +0900

    feat: TodoStore 구현

--- mobx-todo/src/stores/TodoStore.ts
+++ mobx-todo/src/stores/TodoStore.ts
@@ -1,3 +1,25 @@
-const TodoStore = () => {};
+import { observable } from "mobx";
+import { ITodoData } from "../types/TodoType";
+
+interface ITodo {
+  todoData: ITodoData[];
+  currentId: number;
+  addTodo: (content: string) => void;
+  removeTodo: (id: number) => void;
+}
+
+const TodoStore = observable<ITodo>({
+  todoData: [],
+  currentId: 0,
+
+  addTodo(content) {
+    this.todoData.push({ todoId: this.currentId, content, completed: false });
+    this.currentId++;
+  },
+
+  removeTodo(id) {
+    this.todoData.filter(data => data.todoId !== id);
+  },
+});
 
 export default TodoStore;
