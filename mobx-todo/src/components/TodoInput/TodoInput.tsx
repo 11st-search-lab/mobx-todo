4c6ab8a0775982eeead2ae1f7e29f7e09a4509e2
Dami Kim <dami.k.dev@gmail.com>
2021-10-07 00:18:36 +0900

    feat: todo 추가 기능 구현

--- mobx-todo/src/components/TodoInput/TodoInput.tsx
+++ mobx-todo/src/components/TodoInput/TodoInput.tsx
@@ -1,5 +1,28 @@
+import TodoStore from "../../stores/TodoStore";
+import { ITodoData } from "../../types/TodoType";
+import { useState } from "react";
+
 const TodoInput = () => {
-  return <div></div>;
+  const [todoInput, setTodoInput] = useState("");
+
+  const handleAddTodo = () => TodoStore.addTodo(todoInput);
+  const handleTodoInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoInput(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        value={todoInput}
+        onChange={handleTodoInput}
+      />
+      <button type="submit" onClick={handleAddTodo}>
+        추가
+      </button>
+    </div>
+  );
 };
 
 export default TodoInput;
